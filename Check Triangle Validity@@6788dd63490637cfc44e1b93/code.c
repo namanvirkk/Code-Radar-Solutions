100a46d94c0295a500e64dc4fd65a49e949cf1fc
namanvirkk <namanvirk03@gmail.com>
2025-02-18 12:11:31 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a){
+        printf("Valid");
+    }
+    else{
+        printf("Not Valid");
+    }
+    return 0;
+}
