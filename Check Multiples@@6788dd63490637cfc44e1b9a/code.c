837fee2f25d6031e70baf0dcff83bee1755a7ed8
namanvirkk <namanvirk03@gmail.com>
2025-02-18 13:27:16 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -4,15 +4,11 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
     
-    if(b != 0) {  // Check to avoid division by zero
         if(a % b == 0) {
             printf("Yes", a, b);
         } else {
             printf("No", a, b);
         }
-    } else {
-        printf("0");
-    }
 
     return 0;
 }
